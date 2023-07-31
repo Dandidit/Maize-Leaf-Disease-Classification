d088aa954287641307cc03f0b18741d75fedfc3f
Dandidit <120357410+Dandidit@users.noreply.github.com>
2023-08-01 01:19:43 +0800

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,43 +1,91 @@
+import pandas as pd
 import streamlit as st
+import tensorflow as tf
 import cv2
 import numpy as np
-import tensorflow as tf
 
-# Load the trained model
-model = tf.keras.models.load_model('model_accuracy98%(2).h5')
-class_names = ['Blight', 'Common Rust', 'Gray Leaf Spot', 'Healthy']
+# App title
+st.title('Maize Leaf Diseases Identifier Web App')
 
-def classify_image(image):
-    # Resize the image to the desired input shape
-    image = cv2.resize(image, (256, 256))
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+## APP info    
+st.write('''
+
+**This Streamlit App utilizes a Deep Learning model to detect diseases(Northern Leaf Blight,Common Rust,Gray Leaf Spot) that attact the corn leaves, based in digital images.**
+''')
+
+st.text("Upload an image of a maize leaf to classify its disease.")
+
+## load file
+st.sidebar.write("# File Required")
+uploaded_image = st.sidebar.file_uploader('', type=['jpg','png','jpeg'])
+
+################### Class Dict and Dataframe of Probabilites #############################
+# Map class
+map_class = {
+        0:'Northern Leaf Blight',
+        1:'Common Rust',
+        2:'Gray Leaf Spot',
+        3:'Healthy'
+        }
+        
+#Dataframe 
+dict_class = {
+        'Corn Leaf Condition': ['Northern Leaf Blight', 'Common Rust','Gray Leaf Spot','Healthy'],
+        'Confiance': [0,0,0,0]
+        }
+        
+df_results = pd.DataFrame(dict_class, columns = ['Corn Leaf Condition', 'Confiance'])
     
-    image1 = image.astype('uint8')
-    image2 = np.expand_dims(image1, 0)
-    # Make predictions using the model
-    predictions = model.predict(image2)
-    class_index = np.argmax(predictions)
-    confidence = predictions[0][class_index]
+def predictions(preds):
+    df_results.loc[df_results['Corn Leaf Condition'].index[0], 'Confiance'] = preds[0][0]
+    df_results.loc[df_results['Corn Leaf Condition'].index[1], 'Confiance'] = preds[0][1]
+    df_results.loc[df_results['Corn Leaf Condition'].index[2], 'Confiance'] = preds[0][2]
+    df_results.loc[df_results['Corn Leaf Condition'].index[3], 'Confiance'] = preds[0][3]
 
-    return class_names[class_index], confidence
+    return (df_results)          
 
-def main():
-    st.title("Maize Leaf Disease Classification")
-    st.text("Upload an image of a maize leaf to classify its disease.")
+########################################### Load the model #########################
+#@st.cache
+def get_model():
 
-    # Upload image
-    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
+    model = tf.keras.models.load_model("model_accuracy98%(2).h5")
+    #model = tf.keras.models.load_model("model/model_accuracy98%(1).h5")
+    #model = tf.keras.models.load_model("model/modelwofe.h5")
+    #model = tf.keras.models.load_model("model/modelwofe(1).h5")
+    #model = tf.keras.models.load_model("model/modelwofe(2).h5")
+    #model = tf.keras.models.load_model("model/modelwofe(3).h5")
+    return model
 
-    if uploaded_file is not None:
-        # Read the image from the uploader
-        image = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), 1)
-        st.image(image, caption='Uploaded Image', use_column_width=True)
+if __name__=='__main__':
+    
+    # Model
+    model = get_model()
+    
+    # Image preprocessing
+    if not uploaded_image:
+        st.sidebar.write('Please upload an image before preceeding!')
+        st.stop()
+    else:
+        # Decode the image and Predict the class
+        image = cv2.imdecode(np.fromstring(uploaded_image.read(), np.uint8), 1)# Encoding image
+        st.write("## Maize Leaf Image")
+        st.image(image, use_column_width= True) # Display the image
+        #img = tf.io.decode_image(image, channels = 3) # Convert image to tensor
+        img = tf.image.resize(image,(256,256)) # Resize the image
+        #img_arr = tf.keras.preprocessing.image.img_to_array(img) # Convert image to array
+        img_arr = tf.expand_dims(img, 0) # Create a bacth
 
-        # Process the image and get predictions
-        class_name, confidence = classify_image(image)
+    Genrate_pred = st.button("Detect Result") 
+ 
+    if Genrate_pred:
+        st.subheader('Probabilities by Class') 
+        preds = model.predict(img_arr)
+        preds_class = model.predict(img_arr).argmax()
 
-        st.write(f"Prediction: {class_name}")
-        st.write(f"Confidence: {confidence:.2f}")
+        st.dataframe(predictions(preds))
 
-if __name__ == '__main__':
-    main()
+        if (map_class[preds_class]=="Northern Leaf Blight") or (map_class[preds_class]=="Common Rust") or (map_class[preds_class]=="Gray Leaf Spot"): 
+            st.subheader("The Corn Leaf is infected by {} disease".format(map_class[preds_class]))
+
+        else:
+            st.subheader("The Corn Leaf is {}".format(map_class[preds_class]))
